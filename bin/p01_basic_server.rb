9844a96115d0cf58c719032830e9cff4aab0b518
Clay Shaw <Claydshaw@gmail.com>
2018-04-04 15:18:07 -0700

    complete

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -1,1 +1,16 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  path = req.get_header('REQUEST_PATH')
+  res = Rack::Response.new
+  res['Content-Type'] = 'text/html'
+  # just going to output the path
+  res.write(path)
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -8,25 +8,47 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    # @params = route_params.merge(req.params)
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'double render error' if @already_built_response
+
+    @res['Location'] = url
+    @res.status = 302
+    @already_built_response = true
+
+    nil
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise 'double render error' if @already_built_response
+
+    @res['Content-Type'] = content_type
+    @res.write(content)
+    # p @res.body
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    # find controller and template names to construct a path
+    # path = 'views/#{controller_name}/#{template_name}.html.erb'
+    f = File.read(template_name)
+    ERB.new('<%= template %>')
   end
 
   # method exposing a `Session` object
